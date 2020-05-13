b8bf7796eea4f56f516cbab1c521f099cf768f3a
jason-nordheim <jason.nordheim@gmail.com>
2020-05-13 11:24:47 -0600

    defined create params for bus using strong params

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -6,9 +6,16 @@ class BusesController < ApplicationController
     def show 
     end
     def create 
+        @bus = Bus.create(bus_params)
+        render json: @bus
     end 
     def update 
     end 
     def destroy 
+    end 
+
+    private
+    def bus_params
+        params.require(:bus).permit(:tag, :driver,:current_route, :capacity)
     end 
 end

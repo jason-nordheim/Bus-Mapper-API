b210e82aee30344af618a62c0026ca92bd5013f8
jason-nordheim <jason.nordheim@gmail.com>
2020-05-13 11:30:20 -0600

    enabled rout to :create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :buses, except: [:show, :create, :edit, :update, :destroy, :new]
+  resources :buses, except: [:show, :edit, :update, :destroy, :new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

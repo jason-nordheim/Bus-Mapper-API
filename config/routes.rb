fc8b6c260d2afa5260f01070bfcfcc5a39bc46b8
jason-nordheim <jason.nordheim@gmail.com>
2020-05-13 11:20:47 -0600

    initialized, added cors, created resource Bus, added index route for bus

---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  resources :buses, except: [:show, :create, :edit, :update, :destroy, :new]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
